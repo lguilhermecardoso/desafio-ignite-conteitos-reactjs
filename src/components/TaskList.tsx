129200fe81ac7b13760d983b1731e1614318a116
Guilherme Cardoso <guilhermecardoso.info@gmail.com>
2021-03-11 14:50:00 -0300

    complete challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,37 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    let id = Math.floor(Math.random() * 10000);
+    tasks.forEach((task) => {
+      if (task.id === id) {
+        id = Math.floor(Math.random() * 1000);
+      }
+    });
+    const task = {
+      id,
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks(oldState => [...oldState, task])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task )
+
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const task = tasks.filter(t => t.id !== id)
+
+    setTasks(task)
   }
 
   return (
